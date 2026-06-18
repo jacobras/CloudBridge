585dd2a14ce18896a34a5a71b3bf4e132f7d2231
Jacob Ras <jacobras.nl@gmail.com>
2026-06-18 22:09:50 +0200

    Remove unnecessary opt-in

--- demo/shared/src/commonMain/kotlin/nl/jacobras/cloudbridge/demo/ui/FilesList.kt
+++ demo/shared/src/commonMain/kotlin/nl/jacobras/cloudbridge/demo/ui/FilesList.kt
@@ -4,14 +4,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import nl.jacobras.cloudbridge.model.CloudItem
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun FilesList(
     files: List<CloudItem>,
