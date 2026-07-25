4ef3837bf49845cf6bdc47e35f0e391eb52c43f0
Jacob Ras <jacobras.nl@gmail.com>
2026-07-25 10:47:12 +0200

    Fix iOS demo

--- library/src/iosMain/kotlin/nl/jacobras/cloudbridge/auth/startWebFlow.kt
+++ library/src/iosMain/kotlin/nl/jacobras/cloudbridge/auth/startWebFlow.kt
@@ -30,7 +30,7 @@ internal suspend fun startWebFlow(
     redirectUri: String
 ): NSURL? = withContext(Dispatchers.Main) {
     val authUrl = requireNotNull(NSURL(string = url)) { "Invalid URL: $url" }
-    val redirectUriScheme = redirectUri.substringBefore("://")
+    val redirectUriScheme = redirectUri.substringBefore(":/") // Google Drive on iOS uses only one forward slash
 
     suspendCancellableCoroutine { continuation ->
         val contextProvider = PresentationContextProvider()
